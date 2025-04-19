183ed95cceec110f290a8a854624386b21137fa1
osueu <ustundae@oregonstate.edu>
2025-04-19 12:34:19 -0400

    terraform files to deploy to gcp artifacts, cloudrun, and iam

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  default = "hello-world-ct"
+}
+variable "region" {
+  default = "us-east1"
+}
+
+variable "app_name" {
+  default = "hello-world"
+}
+
+variable "repo_id" {
+  default = "hello-world"
+}
